597796104e35d31c3eeca7b0f1880d059a25417a
Chia-Wei Hsu <chiaweih@ucar.edu>
2026-02-11 14:56:40 -0700

    test using pyart for error cfradial1 data

--- test/create_cfradial2_zarr.py
+++ test/create_cfradial2_zarr.py
@@ -1,19 +1,26 @@
 import os
 from glob import glob
-import pyart         # needed for xradar in my env for some reason ()
+import pyart         # needed for xradar in my env for some reason
 import xradar as xd
+import xarray as xr
 
 
 def fix_string_encoding(ds):
-    """Convert fixed-length unicode strings to object dtype for Zarr compatibility"""
+    """
+    Convert fixed-length unicode strings to object dtype for Zarr compatibility
+    print out the variable names and their data types for debugging
+
+    """
     ds = ds.copy()  # Create a mutable copy
     for var in ds.variables:
         if ds[var].dtype.kind == 'U':  # Unicode string
+            print(f"Variable '{var}' converted from {ds[var].dtype} {ds[var].dtype.kind}")
             ds[var] = ds[var].astype(str).astype(object)
+            print(f"to {ds[var].dtype}")
     return ds
 
 if __name__ == "__main__":
-    project_directory = "/lustre/desc1/scratch/chiaweih/SPol.precip/chiaw112212/spol_moments_v2.0_20220525_000000/rhi/20220525/"
+    project_directory = "/lustre/desc1/scratch/chiaweih/SPol.precip/chiaw112212/spol_moments_v2.0_20220525_000000/sur/20220525/"
 
     # get all cfradial1 files
     cfradial1_files = glob(os.path.join(project_directory,'cfrad.*.nc'))
@@ -33,6 +40,9 @@ if __name__ == "__main__":
             dtree = xd.io.open_cfradial1_datatree(file)
         except Exception as e:
             print(f"Error reading {file}: {e}")
+
+            ds_rad = pyart.io.read_cfradial(file)
+            # ds_group = xr.open_dataset(file, group='sweep_0', engine='cfradial1', chunks={})
             continue
 
         # Apply to all nodes in the datatree
